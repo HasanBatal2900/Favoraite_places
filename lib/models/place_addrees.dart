01a5dd5eab13c3739c2a1c1367a4fd299d545f89
HasanBatal2900 <hh0657768@gmail.com>
2023-07-22 14:11:39 -0700

    lat and log for placeAddrees Class

--- lib/models/place_addrees.dart
+++ lib/models/place_addrees.dart
@@ -1,8 +1,14 @@
 class PlaceAddrees {
   // You can Add more details in the below link the follow details of the city from the api
   PlaceAddrees(
-      {required this.city, required this.continent, required this.countryName});
+      {required this.city,
+      required this.continent,
+      required this.countryName,
+      required this.lat,
+      required this.long});
   String continent;
   String city;
   String countryName;
+  double lat;
+  double long;
 }
